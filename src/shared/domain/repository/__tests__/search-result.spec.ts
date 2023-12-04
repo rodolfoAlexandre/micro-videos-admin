d9eda3fe161994e6832545e325da7bf6151d0a15
Rodolfo S Alexandre <r.santos.13@hotmail.com>
2023-12-04 19:55:30 -0300

    tests of search params and search result

---
+++ src/shared/domain/repository/__tests__/search-result.spec.ts
@@ -0,0 +1,42 @@
+import { SearchResult } from "../search-result";
+
+describe("SearchResult Unit Tests", () => {
+  test("constructor props", () => {
+    let result = new SearchResult({
+        items: ["entity1", "entity2"] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 2,
+    })
+
+    expect(result.toJSON()).toStrictEqual({
+        items: ["entity1", "entity2"] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 2,
+        last_page: 2,
+    })
+  })
+
+  test("should set last_page = 1 when per_page field is greater than total field", () => {
+    let result = new SearchResult({
+        items: [] as any,
+        total: 4,
+        current_page: 1,
+        per_page: 15,
+    })
+
+    expect(result.last_page).toBe(1)
+  })
+
+  test("should prop when total is not a multiple of per_page", () => {
+    let result = new SearchResult({
+        items: [] as any,
+        total: 101,
+        current_page: 1,
+        per_page: 20,
+    })
+
+    expect(result.last_page).toBe(6)
+  })
+})
